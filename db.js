fb3532763abb2f69eb9cf64604233351ac05cac3
JPchipres <jchipres@ucol.mx>
2025-03-02 22:33:02 -0600

    credenciales a DB

--- db.js
+++ db.js
@@ -1,17 +1,18 @@
 // db.js
 const mysql = require('mysql2');
-
+const fs = require('fs');
+const env = require('dotenv').config();
 const connection = mysql.createConnection({
   /*host: 'localhost',
   user: 'root', // Cambia esto por tu usuario de MySQL
   password: '', // Cambia esto por tu contraseña de MySQL
   database: 'nattib_salud' // Cambia esto por el nombre de tu base de datos*/
-  host:"nattib-salud-server.mysql.database.azure.com", 
-  user:"qisbhtsron",
-  password: process.env.SECRET_KEY, 
-  database:"nattib_salud_db", 
+  host:'nattib-salud-server.mysql.database.azure.com', 
+  user:'qisbhtsron',
+  password: process.env.PASS_DB, 
+  database:'nattib_salud_db', 
   port:3306, 
-  ssl:{ca:fs.readFileSync("DigiCertGlobalRootCA.crt.pem")}
+  ssl:{ca:fs.readFileSync('DigiCertGlobalRootCA.crt.pem')}
 });
 
 connection.connect((err) => {
