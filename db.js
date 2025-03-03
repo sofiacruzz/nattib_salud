839bae1a28fb3789d7691fd942f99043d1df94f4
JPchipres <jchipres@ucol.mx>
2025-03-02 21:50:14 -0600

    Conexion a azure DB

--- db.js
+++ db.js
@@ -2,10 +2,16 @@
 const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
-  host: 'localhost',
+  /*host: 'localhost',
   user: 'root', // Cambia esto por tu usuario de MySQL
   password: '', // Cambia esto por tu contraseña de MySQL
-  database: 'nattib_salud' // Cambia esto por el nombre de tu base de datos
+  database: 'nattib_salud' // Cambia esto por el nombre de tu base de datos*/
+  host:"nattib-salud-server.mysql.database.azure.com", 
+  user:"qisbhtsron",
+  password:"{your_password}", 
+  database:"{your_database}", 
+  port:3306, 
+  ssl:{ca:fs.readFileSync("DigiCertGlobalRootCA.crt.pem")}
 });
 
 connection.connect((err) => {
