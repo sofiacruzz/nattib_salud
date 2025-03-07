850a230e5d4d1d68dacf50203b2f7aac25e516cc
sofiacruzzz <ccruz4@ucol.mx>
2025-03-06 23:12:37 -0600

    actualizacion del archivo db.js

--- db.js
+++ db.js
@@ -1,18 +1,17 @@
 // db.js
 const mysql = require('mysql2');
 const fs = require('fs');
-const env = require('dotenv').config();
 const connection = mysql.createConnection({
   /*host: 'localhost',
   user: 'root', // Cambia esto por tu usuario de MySQL
   password: '', // Cambia esto por tu contraseña de MySQL
   database: 'nattib_salud' // Cambia esto por el nombre de tu base de datos*/
-  host:'nattib-salud-server.mysql.database.azure.com', 
-  user:'qisbhtsron',
-  password: process.env.PASS_DB, 
+  host:AZURE_MYSQL_HOST, 
+  user:AZURE_MYSQL_USER,
+  password: AZURE_MYSQL_PASSWORD, 
   database:'nattib_salud_db', 
-  port:3306, 
-  ssl:{ca:fs.readFileSync('DigiCertGlobalRootCA.crt.pem')}
+  port:AZURE_MYSQL_PORT, 
+  ssl:AZURE_MYSQL_SSL
 });
 
 connection.connect((err) => {
