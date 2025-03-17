b92bd6c5831db80edd6a9f18ad68b2a0bb961323
JPchipres <jchipres@ucol.mx>
2025-03-16 23:26:03 -0600

    funcionalidad de guardado de informacion

--- public/js/cardpaciente.js
+++ public/js/cardpaciente.js
@@ -57,6 +57,8 @@ inicializarFormulario(
 );
 
 
+
+
 document.addEventListener('DOMContentLoaded', async function () {
     console.log('DOM completamente cargado'); // Paso 1
     const medico_id = localStorage.getItem('medico_id');
@@ -93,4 +95,79 @@ document.addEventListener('DOMContentLoaded', async function () {
     } catch (error) {
         console.error('Error:', error);
     }
+
+    //Logica para guardar la informacion de expediente
+    document.getElementById('guardarBtn1').addEventListener('click', async (event) =>{
+        event.preventDefault();
+
+        const ant_pat = document.getElementById('ant_patologicos_1').value;
+        const no_ant_pat = document.getElementById('ant_nopatologicos_1').value;
+
+        const apiUrl = `${API_URL}medico/update/expediente`;
+
+        try {
+            const response = await fetch(apiUrl, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ 
+                    ant_pat:ant_pat, 
+                    no_pat:no_ant_pat, 
+                    id_expediente:expediente_id
+                })
+                });
+            
+            const data = await response.json();
+            if (data.success){
+                
+            } else {
+                alert('Error al agregar expediente');
+            }
+            
+        } catch (error) {
+            console.log("Error: ", error);
+
+        }
+    });
+
+      //Logica para guardar la informacion de consulta
+      document.getElementById('guardarBtn2').addEventListener('click', async (event) =>{
+        event.preventDefault();
+
+        const padecimiento_actual_2 = document.getElementById('padecimiento_actual_2').value;
+        const exploracion_fisica_2 = document.getElementById('exploracion_fisica_2').value;
+        const diagnostico_2 = document.getElementById('diagnostico_2').value;
+        const tratamiento_2 = document.getElementById('tratamiento_2').value;
+        const estudios_complementarios_2 = document.getElementById('estudios_complementarios_2').value;
+
+        const apiUrl = `${API_URL}medico/update/consulta_medica`;
+
+        try {
+            const response = await fetch(apiUrl, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ 
+                    pad:padecimiento_actual_2, 
+                    exp_fisica:exploracion_fisica_2, 
+                    diag:diagnostico_2,
+                    trat:tratamiento_2,
+                    est_comp:estudios_complementarios_2,
+                    id_consulta:cita_id
+                })
+                });
+            
+            const data = await response.json();
+            if (data.success){
+                
+            } else {
+                alert('Error al agregar cita');
+            }
+            
+        } catch (error) {
+            console.log("Error: ", error);
+        }
+    });
 });
