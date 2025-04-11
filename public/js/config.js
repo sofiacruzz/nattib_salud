776c644da22109bcfcfdcbd153cd4d83a3ebe9f6
JPchipres <jchipres@ucol.mx>
2025-04-10 23:34:42 -0600

    cambio de ambiente

--- public/js/config.js
+++ public/js/config.js
@@ -1,2 +1,2 @@
-const API_URL = "http://localhost:3000/";
-//const API_URL = "https://nattib-salud.azurewebsites.net/";
+//const API_URL = "http://localhost:3000/";
+const API_URL = "https://nattib-salud.azurewebsites.net/";
