46f101e566412912905c72e07c4d3b8a4e9c5b6f
sofiacruzzz <ccruz4@ucol.mx>
2025-04-14 00:45:28 -0600

    fixes

--- public/js/config.js
+++ public/js/config.js
@@ -1,2 +1,2 @@
 //const API_URL = "http://localhost:3000/";
-const API_URL = "inherasalud-hpbmcud0ffe2hcdh.eastus2-01.azurewebsites.net";
+const API_URL = "inherasalud-hpbmcud0ffe2hcdh.eastus2-01.azurewebsites.net/";
