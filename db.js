bca04b1e0ba75d2334a0f5d46afc548de7fee12e
sofiacruzzz <ccruz4@ucol.mx>
2025-04-13 22:39:29 -0600

    cambios

--- db.js
+++ db.js
@@ -4,13 +4,13 @@ import fs from 'fs';
 import dotenv from 'dotenv';
 
 dotenv.config();
-const serverCa = [fs.readFileSync("DigiCertGlobalRootCA.crt.pem", "utf8")];
+const serverCa = [fs.readFileSync("DigiCertGlobalRootG2.crt.pem", "utf8")];
 
 var connection = mysql.createConnection({
   host: process.env.AZURE_MYSQL_HOST,
   user: process.env.AZURE_MYSQL_USER,
   password: process.env.AZURE_MYSQL_PASSWORD,
-  database: 'nattib_salud_db',
+  database: 'nattib-salud-database',
   port: 3306,
   ssl: {
     ca: serverCa,
