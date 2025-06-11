7702e9718f703867c4a80fb62f5ba19a25600302
sofiacruzzz <ccruz4@ucol.mx>
2025-06-10 21:29:36 -0600

    ver contraseña

--- public/js/script.js
+++ public/js/script.js
@@ -11,6 +11,20 @@ function closeForm() {
       document.body.style.overflow = '';
 
 }
+function togglePassword(inputId) {
+    const input = document.getElementById(inputId);
+    const icon = input.nextElementSibling.querySelector('i');
+    
+    if (input.type === "password") {
+        input.type = "text";
+        icon.classList.remove('fa-eye');
+        icon.classList.add('fa-eye-slash');
+    } else {
+        input.type = "password";
+        icon.classList.remove('fa-eye-slash');
+        icon.classList.add('fa-eye');
+    }
+}
 
 // Inicializa Pikaday
 const picker = new Pikaday({
